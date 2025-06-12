d7b4c3da0682f275d095bfab1504b04c200f8290
Laura Shepherd <lshepherd@dwolla.com>
2025-06-12 14:13:48 -0500

    Test new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nolanlovesdada/',  // This should match your repository name
+  base: './',  // This should match your repository name
 }); 
