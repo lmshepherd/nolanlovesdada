40b197db02f6dd5d5c8a20aac1cbc9b1d08c3143
Laura Shepherd <lshepherd@dwolla.com>
2025-06-12 21:40:24 -0500

    Fix build issues

--- fatherly-madlibs/src/utils/isFormValid.ts
+++ fatherly-madlibs/src/utils/isFormValid.ts
@@ -2,6 +2,6 @@ import type { MadLibInputs } from '../types/MadLibInputs';
 
 export const isFormValid = (inputs: MadLibInputs): boolean => {
   return Object.values(inputs).every((section) =>
-    Object.values(section).every((value) => value.trim() !== '')
+    Object.values(section).every((value) => (value as string).trim() !== '')
   );
 }; 
